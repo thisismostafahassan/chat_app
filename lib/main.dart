2081e78ea09d173f6d7c0b4ff4296cef565cf6da
thisismostafahassan <iammostafahassan@gmail.com>
2025-05-14 16:38:48 +0300

    connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'dart:async';
 import 'dart:developer';
+
 import 'package:chat_app/core/themes/themes.dart';
 import 'package:chat_app/features/home/presentation/bloc/home_bloc.dart';
 import 'package:chat_app/features/sign/presentation/pages/login_page.dart';
+import 'package:chat_app/firebase_options.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:chat_app/core/helper/app_bloc_observer.dart';
 import 'package:chat_app/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'features/sign/presentation/bloc/sign_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   runZonedGuarded(
     () {
       //
       WidgetsFlutterBinding.ensureInitialized();
+      //
+      Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
       //
       Bloc.observer = AppBlocObserver();
       //
@@ -26,7 +30,6 @@ void main() async {
           designSize: const Size(1080, 1920),
           minTextAdapt: true,
           splitScreenMode: true,
-
           child: MultiBlocProvider(
             providers: [
               BlocProvider(create: (context) => HomeBloc()),
